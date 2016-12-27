1319b7c2738eb48a6b1a23eb39cbeca30e5df8de
Koen van Vliet <koen9410@gmail.com>
2016-12-27 13:41:37 +0100

    Get tile ID from tile image instead of gui.

--- tmx2appv.py
+++ tmx2appv.py
@@ -11,6 +11,11 @@ from int_utils import *
 # Name of the layer which holds our tilemap data
 tile_layer_name = "Tile Layer 1"
 
+# Define tileset size here
+class Tileset:
+	width = 16
+	# height = None
+
 # Define room size here
 class Room:
 	width = 11
@@ -30,7 +35,7 @@ class bcol:
     ENDC = '\033[0m'
 
 # TODO: Change to variable instead of hardcoded filename
-tmxdata = tm.TiledMap('map.tmx')
+tmxdata = tm.TiledMap('herocore-mapv30.tmx')
 
 t = tmxdata.get_layer_by_name(tile_layer_name)
 
@@ -71,11 +76,14 @@ for wy in range(0, World.height):
 			for rx in range(0, Room.width):
 				x = wx * Room.width + rx
 				y = wy * Room.height + ry
-
 				# TODO: This layer number should not be fixed!
-				gid = tmxdata.get_tile_gid(x, y, 0)
-				tileId = gid - 1
+				# TODO: Unused tiles are apparently excluded from global tile ID pool.
+				# This is a temporary workaround for this issue. Can only be used in this
+				# specific case
+				image = tmxdata.get_tile_image(x, y, 0)
+				tx = int(image[1][0] / 8)
+				ty = int(image[1][1] / 8)
+				tileId = ty * Tileset.width + tx
 
 				f.write(int2b8(tileId))
-
 f.close()
